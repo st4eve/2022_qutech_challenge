2483bab7d4b0199592c085af2ad631a66a9e4f93
Juni <hyun.lee1@rwth-aachen.de>
2022-01-30 06:17:49 +0900

    image read

--- opencv.py
+++ opencv.py
@@ -1,6 +1,8 @@
 
 import cv2
 from pyzbar import pyzbar
+import numpy as np
+
 
 # read QR code and save the code in a file(QPcode_result.txt)
 def read_QRcodes(frame):
@@ -11,10 +13,17 @@ def read_QRcodes(frame):
         QRcode_info = QRcode.data.decode('utf-8')
         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
         with open("QRcode_result.txt", mode='w') as file:
-            file.write("Recognized QRcode:" + QRcode_info)
+            file.write(QRcode_info)
     return frame
 
+
 # read the medical image with the QR coded name
+def get_image(coded_name):
+    file_name = str(coded_name)+".jpeg"
+    img = cv2.imread(file_name, cv2.IMREAD_GRAYSCALE)
+    cv2.imshow('image', img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
 
 # get data from camera
